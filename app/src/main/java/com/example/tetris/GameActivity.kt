210262f9c566601ede2cfe96971e3d0e05d7a7f3
Artyom <goglakk2@gmail.com>
2025-02-18 20:55:26 +0200

    Listeners for buttons, SharedPreferences, GameActivity

---
+++ app/src/main/java/com/example/tetris/GameActivity.kt
@@ -0,0 +1,20 @@
+package com.example.tetris
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.tetris.databinding.ActivityGameBinding
+import com.example.tetris.databinding.ActivityMainBinding
+
+class GameActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityGameBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityGameBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+    }
+}
--- app/src/main/java/com/example/tetris/MainActivity.kt
+++ app/src/main/java/com/example/tetris/MainActivity.kt
@@ -1,19 +1,37 @@
 package com.example.tetris
 
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.tetris.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
+    private lateinit var preferences: AppPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        preferences = AppPreferences(this)
         setContentView(binding.root)
 
+        with(binding){
+            btnExit.setOnClickListener{
+                finish()
+            }
+            btnNewGame.setOnClickListener{
+                val intent: Intent = Intent(this@MainActivity, GameActivity::class.java)
+                startActivity(intent)
+            }
+            btnResetScore.setOnClickListener{
+                preferences.clearHighScore()
+                Snackbar.make(binding.root, "Score was successfully reset", Snackbar.LENGTH_SHORT).show()
+            }
+
+        }
     }
 }
